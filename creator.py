7e1ddf579fc49951979b6d06ee97a79a5d02f2d9
Sirui Sun <siruisun@google.com>
2021-09-15 16:55:48 -0700

    first impl of creator script

--- creator.py
+++ creator.py
@@ -1,21 +1,125 @@
 import requests, json, time
 from pprint import pprint
 from googleapiclient import discovery
+from googleapiclient.errors import HttpError
 from oauth2client.client import GoogleCredentials
-
-# reads config file and creates VMs if they don't already exist
 project_id = "sirui-sun-test-project"
-config_file = open('config', 'r')
-configs = config_file.readlines()[1:]
 
 # check if the VM exists
 def vmExists(zone, name):
-	# get maintenance notice from this instance's API endpoint
+	try:
+		credentials = GoogleCredentials.get_application_default()
+		service = discovery.build('compute', 'alpha', credentials=credentials)
+		request = service.instances().get(project=project_id, zone=zone, instance=name)
+		response = request.execute()
+		return True
+	except HttpError as err:
+		if err.resp.status == 404:
+			return False
+		else: raise
+
+
+# create a VM instance with periodic maintenance intervals
+def createPeriodicMaintenanceVM(name, zone, machine_shape, gpu, lssd):
 	credentials = GoogleCredentials.get_application_default()
 	service = discovery.build('compute', 'alpha', credentials=credentials)
-	request = service.instances().get(project=project_id, zone=zone, instance=name)
-	response = request.execute()
-	pprint(response)
+	region = str.join("-", zone.split("-")[:-1])
+	body = {
+	  "name": name,
+	  "machineType": "projects/{0}/zones/{1}/machineTypes/{2}".format(project_id, zone, machine_shape),
+	  "guestAccelerators": [],
+	  "disks": [
+	    {
+	      "kind": "compute#attachedDisk",
+	      "type": "PERSISTENT",
+	      "boot": True,
+	      "mode": "READ_WRITE",
+	      "autoDelete": True,
+	      "deviceName": name,
+	      "initializeParams": {
+	        "sourceImage": "projects/sirui-sun-test-project/global/images/sf-worker",
+	        "diskType": "projects/{0}/zones/{1}/diskTypes/pd-balanced".format(project_id, zone),
+	        "diskSizeGb": "10"
+	      },
+	      "diskEncryptionKey": {}
+	    }
+	  ],
+	  "canIpForward": False,
+	  "networkInterfaces": [
+	    {
+	      "kind": "compute#networkInterface",
+	      "subnetwork": "projects/{0}/regions/{1}/subnetworks/default".format(project_id, region),
+	      "accessConfigs": [
+	        {
+	          "kind": "compute#accessConfig",
+	          "name": "External NAT",
+	          "type": "ONE_TO_ONE_NAT",
+	          "networkTier": "PREMIUM"
+	        }
+	      ],
+	      "aliasIpRanges": []
+	    }
+	  ],
+	  "description": "",
+	  "labels": {
+	  	"stable-fleet-test": ""
+	  },
+	  "scheduling": {
+	    "preemptible": False,
+	    "onHostMaintenance": "TERMINATE",
+	    "automaticRestart": True,
+	    "maintenanceInterval": "PERIODIC",
+	    "nodeAffinities": []
+	  },
+	  "deletionProtection": False,
+	  "reservationAffinity": {
+	    "consumeReservationType": "ANY_RESERVATION"
+	  },
+	  "serviceAccounts": [
+	    {
+	      "email": "806547283812-compute@developer.gserviceaccount.com",
+	      "scopes": [
+	        "https://www.googleapis.com/auth/cloud-platform"
+	      ]
+	    }
+	  ],
+	  "shieldedInstanceConfig": {
+	    "enableSecureBoot": False,
+	    "enableVtpm": True,
+	    "enableIntegrityMonitoring": True
+	  },
+	}
+
+	# TODO: allow configuration by # of GPUs
+	if(gpu != "none"):
+		body.guestAccelerators = [
+	    	{
+	      		"acceleratorCount": 1,
+	      		"acceleratorType": "projects/{0}/zones/{1}/acceleratorTypes/{2}".format(project_id,zone,gpu)
+	    	}
+		]
+
+	# TODO: allow configuration by # of lssds
+	if(lssd != "none"):
+		lssd_body = {
+			"autoDelete": true,
+			"deviceName": "local-ssd-0",
+			"initializeParams": {
+				"diskType": "projects/{0}/zones/{1}/diskTypes/local-ssd".format(project_id,zone)
+			},
+			"interface": "NVME",
+			"mode": "READ_WRITE",
+			"type": "SCRATCH"
+		}
+		body.disks.append(lssd_body)
+
+	request = service.instances().insert(project=project, zone=zone, body=body)
+	request.execute()
+	print("created {0}".format(name))
+
+# reads config file and creates VMs if they don't already exist
+config_file = open('config', 'r')
+configs = config_file.readlines()[1:]
 
 # Strips the newline character
 for config in configs:
@@ -24,6 +128,11 @@ for config in configs:
 	zone = config[1]
 	machine_shape = config[2]
 	gpu = config[3]
-	ssd = config[4]
+	lssd = config[4]
 
-	print(vmExists(zone, name))
+	if(vmExists(zone, name)):
+		print("skipping {0}: VM instances exists".format(name)
+	else:
+		createPeriodicMaintenanceVM(name, zone, machine_shape, gpu, lssd)
+
+
